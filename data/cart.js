37490a611e453571447b1505042b57bfeeda311d
duality <yashbhurke69@gmail.com>
2025-07-03 15:20:52 +0530

    Move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export let cart = []
+
+export function addToCart(productId) {
+  let matchingItem = undefined
+  cart.forEach(cartItem => {
+    if(productId === cartItem.productId) {
+      matchingItem = cartItem
+    }
+  })
+
+  if(matchingItem) {
+    matchingItem.quantity++
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    }) 
+  }
+}
+
