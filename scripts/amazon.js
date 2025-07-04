012806fd05f597789cd4757b3f055d258eeef2ef
duality <yashbhurke69@gmail.com>
2025-07-04 20:47:10 +0530

    14e

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -61,10 +61,7 @@ products.forEach(product => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
 function updatecartQuantity() {
-  let cartQuantity = 0
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity
-  })
+  const cartQuantity = calculateCartQuantity()
   const cartQuantityElement = document.querySelector('.js-cart-quantity')
   if(cartQuantity === 0) {
     cartQuantityElement.innerHTML = ''
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {calculateCartQuantity, cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -108,10 +108,7 @@ document.querySelectorAll('.js-delete-link').forEach(link => {
   }) 
 
 function updateCartQuantity() {
-  let cartQuantity = 0
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity
-    })
+  const cartQuantity = calculateCartQuantity()
   document.querySelector('.js-return-to-home').innerHTML = `${cartQuantity} items`
 }
 updateCartQuantity()    //when loading
